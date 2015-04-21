813b33e1990df6738a93f8bbb71b3b4a3f325101
Rafael Nossal <rafael.nossal@gmail.com>
2015-04-21 02:27:08 -0300

    Adicionado o texto do problema

--- folha/selecao.php
+++ folha/selecao.php
@@ -1,4 +1,17 @@
 <?php
+/*
+Sabe-se que por trás de cada cometa há um OVNI. Esses OVNIs frequentemente buscam bons desenvolvedores aqui na Terra. Infelizmente só têm espaço para levar um grupo de devs por vez. Para a seleção, há um engenhoso esquema, da associação do nome do cometa ao nome do grupo, que possibilita a cada grupo saber se será levado ou não.
+Os dois nomes, do grupo e do cometa, são convertidos em um número que representa o produto das letras do nome, onde "A" é 1 e "Z" é 26. Assim, o grupo "LARANJA" seria 12 * 1* 18 * 1 * 14 * 10 * 1 = 30240. Se o resto da divisão do número do grupo por 45 for igual ao resto da divisão do número do cometa por 45, então o grupo será levado.
+Para os cometas e grupos abaixo, qual grupo NÃO será levado?
+
+COMETA		GRUPO
+--------------------
+HALLEY		AMARELO
+ENCKE		VERMELHO
+WOLF		PRETO
+KUSHIDA		AZUL
+*/
+
 $cometaCores = [
 	"halley" => "amarelo",
 	"encke" => "vermelho",
